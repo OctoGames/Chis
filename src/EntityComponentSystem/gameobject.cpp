ed4d7143606a77ca716fea7be03fd2f98ee2c86e
Néstor Cabrero <nestor.cabrero@ucm.es>
2019-03-12 10:33:23 +0100

    Simplified ECS add system

--- src/EntityComponentSystem/gameobject.cpp
+++ src/EntityComponentSystem/gameobject.cpp
@@ -1,4 +1,5 @@
 #include "gameobject.h"
+#include "entitycomponentmanager.h"
 
 int GameObject::game_object_count_ = 0;
 
@@ -9,6 +10,7 @@ GameObject::GameObject(const std::string& name, const std::string& tag, bool act
 	unique_id_(game_object_count_)
 {
 	game_object_count_++;
+	if (tag_ != "") EntityComponentManager::Instance()->addGameObjectWithTag(this, tag_);
 }
 
 GameObject::~GameObject()
