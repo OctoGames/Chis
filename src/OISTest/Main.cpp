3437b992d6e5cf553d9f99e418c21e5b28161eec
Néstor Cabrero <nestor.cabrero@ucm.es>
2019-03-19 16:26:08 +0100

    OIS Test Added

---
+++ src/OISTest/Main.cpp
@@ -0,0 +1,20 @@
+#include "Application.h"
+
+int main(int argc, char *argv[])
+{
+	Application* app = nullptr;
+
+	try
+	{
+		app = new Application();
+		app->run();
+	}
+	catch (Ogre::Exception& e)
+	{
+		Ogre::LogManager::getSingleton().logMessage("An exception has occured: " + e.getFullDescription() + "\n");
+	}
+
+	delete app;
+
+	return 0;
+}
