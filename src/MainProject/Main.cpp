928dcebd1103ef751161d51a7397015b5407462a
Unknown <gonalba@ucm.es>
2019-03-19 16:30:59 +0100

    The materials are loaded well

--- src/MainProject/Main.cpp
+++ src/MainProject/Main.cpp
@@ -1,5 +1,5 @@
 #include "Application.h"
-
+#include "OgreSystem.h"
 //Garbage collector
 //#include "CheckML.h"
 
@@ -7,9 +7,18 @@ int main(int argc, char* argv[])
 {
 	//Garbage collector
 	//_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-	Application* app = new Application();
-	app->run();
-	delete app;
+	Application* app = nullptr;
 
+	try
+	{
+		app = new Application();
+		app->run();
+	}
+	catch (Ogre::Exception& e)
+	{
+		Ogre::LogManager::getSingleton().logMessage("An exception has occured: " + e.getFullDescription() + "\n");
+	}
+
+	delete app;
 	return 0;
 }
